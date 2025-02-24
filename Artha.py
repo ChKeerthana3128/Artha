31808eb759c093cffef492c5eaa25b0e1af44448
ChKeerthana3128 <choutakurikeerthana3182005@gmail.com>
2025-02-24 14:20:48 +0530

    Update Artha.py

--- Artha.py
+++ Artha.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
 # UI Configuration
-st.set_page_config(page_title="AI-Based Financial Dashboard", layout="wide")
+st.set_page_config(page_title="Artha", layout="wide")
 
 # Load dataset with error handling
 @st.cache_data
